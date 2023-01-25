d71aa70da6e37c4878c0afa26f8a35139fc16915
LAPTOP-P8B1SVC3\Przemek <przemoc992@gmail.com>
2023-01-25 13:26:50 +0100

    Inicjalizacja repo

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h1>Fib-Calculator</h1>
+      <h3>Przemysław Borczuk I2S 2.1</h3>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
